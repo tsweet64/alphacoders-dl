e17c0bb863ce8407b47167bcfe6e716e9213a3aa
Tim Sweet <tsweet64@protonmail.com>
2021-10-20 00:29:18 -0500

    some improvements to error handling, etc

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ extern crate select;
 use regex::Regex;
 use select::document::Document;
 use select::predicate::Attr;
-use std::{env, fs, io, vec};
+use std::{env, fs, io};
+use anyhow::Result;
+use anyhow::Context;
 
 struct ImageItem {
     data_id: String,
@@ -13,43 +15,25 @@ struct ImageItem {
     data_server: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
     //get the url from command line arguments
-    let args: Vec<String> = env::args().collect();
-    let input_url = match args.len() {
-        1 => panic!("Please specify a valid url."),
-        _ => &args[1],
-    };
+    let input_url = env::args().nth(1).context("Please specify a valid url.")?;
     //get base url. This does nothing for now but will eventually extract the correct url from the different variations
     let base_url = get_base_url(input_url);
 
     //get parseable document from the given base_url
-    let html_document = match get_html_from_url(&base_url) {
-        Ok(page) => match page {
-            Ok(page) => page,
-            Err(e) => panic!("Unable to continue. Could not download the HTML {:?}", e),
-        },
-        Err(e) => panic!("Unable to continue. Could not parse the HTML {:?}", e),
-    };
+    let html_document = get_html_from_url(&base_url)?;
     //get the total number of pages of wallpapeprs
-    let total_pages = find_total_pages(&html_document);
+    let total_pages = find_total_pages(&html_document).context("Could not parse page count as integer")?;
 
-    for page in 1..=match total_pages {
-        Ok(total_pages) => total_pages,
-        Err(e) => panic!("Cannot parse the page count as integer {:?}", e),
-    } {
-        let page_url = base_url.to_string() + "&page=" + &page.to_string();
-
+    for page in 1..=total_pages {
+        let page_url = format!("{}&page={}", base_url, page);
+        
+        //we currently skip a page if there is an error.
         let current_page = match get_html_from_url(&page_url) {
-            Ok(item) => match item {
-                Ok(item) => item,
-                Err(e) => {
-                    eprintln!("Skipped page {}; Unable to download page: {:?}", page, e);
-                    continue;
-                }
-            },
+            Ok(item) => item,
             Err(e) => {
-                eprintln!("Skipped page {}; Unable to parse page HTML: {:?}", page, e);
+                eprintln!("Skipped page {} due to error: {:?}", page, e);
                 continue;
             }
         };
@@ -58,33 +42,21 @@ fn main() {
         }
     }
 
-    //println!("{}", html_text);
+    Ok(())
 }
 
-fn get_html_from_url(url: &str) -> Result<Result<Document, io::Error>, reqwest::Error> {
-    match reqwest::blocking::get(url) {
-        Ok(page) => Ok(Document::from_read(page)),
-        Err(e) => Err(e),
-    }
+fn get_html_from_url(url: &str) -> Result<Document> {
+    Ok(Document::from_read(reqwest::blocking::get(url)?)?)
 }
 
-fn find_total_pages(html: &Document) -> Result<u32, std::num::ParseIntError> {
-    let html_span = match html.find(Attr("class", "btn btn-info btn-lg")).next() {
-        Some(html_span) => html_span.text(),
-        None => panic!("Unable to parse the page count (html error). Cannot continue."),
-    };
-    let page_count = match Regex::new(r"/(?P<cnt>\d+)").unwrap().captures(&html_span) {
-        Some(page_count) => match page_count.name("cnt") {
-            Some(page_count) => page_count.as_str(),
-            None => panic!("Unable to parse the page count (regex failure). Cannot continue."),
-        },
-        None => panic!("Unable to parse the page count (regex failure). Cannot continue."),
-    };
-    return page_count.parse::<u32>();
+fn find_total_pages(html: &Document) -> Result<u32> {
+    let html_span = html.find(Attr("class", "btn btn-info btn-lg")).next().context("Unable to parse the page count (html error). Cannot continue.")?.text();
+    let page_count = Regex::new(r"/(?P<cnt>\d+)").unwrap().captures(&html_span).context("Unable to parse the page count (regex failure)")?.name("cnt").context("Unable to parse the page count (regex failure)")?.as_str();
+    Ok(page_count.parse::<u32>()?)
 }
 
 fn get_all_page_items(html: &Document) -> Vec<ImageItem> {
-    let mut result = vec![];
+    let mut result = Vec::new();
     for item in html.find(Attr("class", "btn btn-primary btn-block download-button")) {
         result.push(ImageItem {
             data_id: match item.attr("data-id") {
@@ -105,13 +77,8 @@ fn get_all_page_items(html: &Document) -> Vec<ImageItem> {
 }
 
 fn get_image(item: &ImageItem) {
-    let image_download_url = String::from("https://initiate.alphacoders.com/download/wallpaper/")
-        + &item.data_id
-        + "/"
-        + &item.data_server
-        + "/"
-        + &item.data_type;
-    let image_output_filename = String::from(&item.data_id) + "." + &item.data_type;
+    let image_download_url = format!("https://initiate.alphacoders.com/download/wallpaper/{}/{}/{}", &item.data_id, &item.data_server, &item.data_type);
+    let image_output_filename = format!("{}.{}", &item.data_id, &item.data_type);
     println!("Downloading image to {}", image_output_filename);
     //TODO how do we propagate these errors upwards so we only have to skip this particular item if there's an error?
     let mut w_resp = match reqwest::blocking::get(image_download_url) {
@@ -125,7 +92,7 @@ fn get_image(item: &ImageItem) {
     io::copy(&mut w_resp, &mut outfile).expect("Failed to write image content into file.");
 }
 
-fn get_base_url(url: &str) -> &str {
+fn get_base_url(url: String) -> String {
     // TODO: parse other types of input urls correctly:
     url
 }
